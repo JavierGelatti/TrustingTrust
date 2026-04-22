803d2d54b2dc87d9b8e4c4b996c8469b30c9c791
Facundo Javier Gelatti <javiergelatti@gmail.com>
2026-04-22 12:41:27 -0300

    Mejorar run.sh de la primera etapa

--- 0/run.sh
+++ 0/run.sh
@@ -5,4 +5,13 @@ set -euo pipefail
 clear
 ./compiler programa.c -o programa
 
-./programa > salida.txt
+# Podría verse el diff, pero en general resulta más claro simplemente ver todo el código generado:
+./programa
+
+# Para ejecutar esto automáticamente:
+#watchexec -c -e c ./run.sh
+
+# Para mostrar el diff:
+#./programa > salida.txt
+#diff --color programa.c salida.txt
+
